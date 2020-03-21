6176408a0790c64d240c856975548fac94105cb8
Akida31 <47895118+Akida31@users.noreply.github.com>
2020-03-21 19:21:48 +0100

    switched to new asset directory
    neccessary file to provide dynamic icon loading

---
+++ src/components/Sidebar/icons.js
@@ -0,0 +1,35 @@
+import alertcircleactive from '../../Assets/icons/alert-circle-active.svg';
+import alertcircle from '../../Assets/icons/alert-circle.svg';
+import checkactive from '../../Assets/icons/check-active.svg';
+import check from '../../Assets/icons/check.svg';
+import editactive from '../../Assets/icons/edit-active.svg';
+import edit from '../../Assets/icons/edit.svg';
+import homeactive from '../../Assets/icons/home-active.svg';
+import home from '../../Assets/icons/home.svg';
+import listactive from '../../Assets/icons/list-active.svg';
+import list from '../../Assets/icons/list.svg';
+import pluscircleactive from '../../Assets/icons/plus-circle-active.svg';
+import pluscircle from '../../Assets/icons/plus-circle.svg';
+import settingsactive from '../../Assets/icons/settings-active.svg';
+import settings from '../../Assets/icons/settings.svg';
+import useractive from '../../Assets/icons/user-active.svg';
+import user from '../../Assets/icons/user.svg';
+
+export default ({
+    "alert-circle-active.svg": alertcircleactive,
+    "alert-circle.svg": alertcircle,
+    "check-active.svg": checkactive,
+    "check.svg": check,
+    "edit-active.svg": editactive,
+    "edit.svg": edit,
+    "home-active.svg": homeactive,
+    "home.svg": home,
+    "list-active.svg": listactive,
+    "list.svg": list,
+    "plus-circle-active.svg": pluscircleactive,
+    "plus-circle.svg": pluscircle,
+    "settings-active.svg": settingsactive,
+    "settings.svg": settings,
+    "user-active.svg": useractive,
+    "user.svg": user,
+});
