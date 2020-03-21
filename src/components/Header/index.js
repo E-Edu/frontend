a38e54b841d3d0d92c46174ad9cab43a44849018
Stefan <#Stefan2006>
2020-03-21 20:26:52 +0100

    Fix Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,6 @@ class Header extends Component {
 						</div>
 					</div>
 				);
-				break;
 			case 'Dashboard':
 					return (
 						<div className='header'>
@@ -47,18 +46,8 @@ class Header extends Component {
 									<img src={Logout_Image} alt="Logout"/>
 								</button>
 							</div>
-						</div>
-						<div id='Button_Box_Dashboard'>
-							<button className='BTN_Dash' id='Button_Info'>
-								<img src='assets/helpcircle.svg' alt='Help' />
-							</button>
-							<button className='BTN_Dash' id='Button_Logout'>
-								<img src='assets/logout.svg' alt='Logout' />
-							</button>
-						</div>
 					</div>
 				);
-				break;
 		}
 		return null;
 	}
