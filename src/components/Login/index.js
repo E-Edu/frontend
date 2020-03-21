603d67d24eb36adce19dd39c2238fdc26ef648f0
Lea <LeaLaufner@t-online.de>
2020-03-21 10:28:23 +0100

    Edited login; i moved the "script function changevisibility" up and deleted the script tag and "function"

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,30 +6,51 @@ class Template extends Component {
 		super(props);
 	}
 	state = {};
+	
+	changeVisibility(){
+		let x=document.getElementById("passwort");
+		let y=document.getElementById("hide1");
+		let z=document.getElementById("hide2");
+		
+		if(x.type ==='password'){
+			x.type="text";
+			y.style.display = "block";
+			z.style.display = "none";
+		} else{
+			x.type="password";
+			y.style.display = "none";
+			z.style.display = "block";
+			
+		}
+	
+	};
+
 
 	render() {
-		return <div><form class="box" action = "loginPop.html" method="post">
-        <div class="boxinhalt">
+		return (<div><form className="box" action = "loginPop.html" method="post">
+        <div className="boxinhalt">
     
         <h1 id = "Headline"> L O G I N</h1>
-        <div class="input-box">
-        <input type="text" name="" placeholder="Enter Email..." class="input-field" required></input>
+        <div className="input-box">
+        <input type="text" name="" placeholder="Enter Email..." className="input-field" required></input>
             </div>
      
-        <div class="input-box"> 
+        <div className="input-box"> 
            
-            <input type="password" value="" id="passwort" placeholder="Enter Password..."  class="input-field" required></input>
+            <input type="password" value="" id="passwort" placeholder="Enter Password..."  className="input-field" required></input>
             
-            <span class="eye" onclick="changeVisibility()">
-                <i id="hide1"class="fa fa-eye"></i>
-                <i id="hide2"class="fa fa-eye-slash" ></i>
+            <span className="eye" onclick="changeVisibility()">
+                <i id="hide1"className="fa fa-eye"></i>
+                <i id="hide2"className="fa fa-eye-slash" ></i>
             </span>
 
         </div>
         <button id="google-login"><img id="googleBild" src="google-logo.png"></img> Log In with Google </button>
-        <input type="submit" name="" id= "login" class="button" value="Log In"></input>         
+        <input type="submit" className="" id= "login" className="button" value="Log In"></input>         
       </div>
-    </form></div>;
+    </form>
+
+	</div>);
 	}
 }
 
