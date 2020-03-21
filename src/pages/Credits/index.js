077abcf3ec1f732f7666ee0be769e07a4f36ac8c
Stefan <#Stefan2006>
2020-03-21 11:45:58 +0100

    Add Credits-Imprint Folder/js --

---
+++ src/pages/Credits/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Credits extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {};
+
+    render() {
+        return <div className='credits'></div>;
+    }
+}
+
+export default Credits;
---
+++ src/pages/Imprint/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Imprint extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {};
+
+    render() {
+        return <div className='imprint'></div>;
+    }
+}
+
+export default Imprint;
