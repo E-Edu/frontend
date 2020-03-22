e70190b07c88024a55e608dfc316d7756fbc82c3
Harry <harry5.ppj@gmail.com>
2020-03-22 18:26:50 +0100

    ReportInfo
    Co-authored-by: colinseelbach@gmail.com <colinseelbach@gmail.com>
    Co-authored-by: sebastian.sturm1988@hotmail.com <sebastian.sturm1988@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ import Imprint from "./pages/Imprint";
 import Privacy from "./pages/Privacy";
 import Credits from "./pages/Credits";
 import ReportsList from "./pages/Reports/List";
-import Sidebar from "./components/Sidebar";
+//import Sidebar from "./components/Sidebar";
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
-			<Router>
-                <Sidebar/>
+			<Router>                
                     <Route path={['/login', '/register', '/']} exact component={Landing} />
                     <Route path='/task' component={Task} />
                     <Route path='/dashboard' component={Dashboard} />
