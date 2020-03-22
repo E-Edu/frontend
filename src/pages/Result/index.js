eb2fd2bdc4b6c5820b215255aaf0c0d8e8d26985
Adrian Görisch <adrian.goerisch@10m.de>
2020-03-22 10:00:40 +0100

    add array for colors

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.scss';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import ResultQuestion from '../../components/ResultQuestion';
 
 
@@ -11,11 +10,12 @@ class Result extends React.Component {
     }
 
 
+    results = ['#BA1919', '#19BA3f', '#687D9A', '#687D9A', '#19BA3f'];
+
     render() {
         return (
             <div className='result'>
-                <Header side='Dashboard' />
-                
+                <Header side='Dashboard'/>
                 <div className='resultContainer'>
                     <div className='resultContentHeader'>
                         <div className='resultLeft'>
@@ -29,39 +29,31 @@ class Result extends React.Component {
                         <div className='resultRight'>
                             <div className='resultPoints'>
                                 <p>icon </p>
-                                
                                 <p><span> 213</span> Punkte</p>
-
                             </div>
                             <div className='resultPoints'>
-                            <p>icon </p>
-                            <p><span> 21.323</span> Punkte</p>
+                                <p>icon </p>
+                                <p><span> 21.323</span> Punkte</p>
                             </div>
                         </div>
-
                     </div>
                     <div className='resultEvaluation'>
                         <span>Auswertung</span>
                         <p>Korrekte Fragen:<span> 7 von 10 </span></p>
-                        
                     </div>
                     <div className='resultPointcount'>
-                        
                         <div className='resultNewPoints'>+7 Punkte</div>
-                        
                         <div className='resultWeeklyPoints'>
                             <p>Diese Woche: <span> 22 Punkte</span></p>
-
                         </div>
-
-
                     </div>
                     <div className='resultQuestions'>
-                        <ResultQuestion></ResultQuestion>
+                        {this.results.map(((value, index) => {
+                            return <div className="resultQuestion" key={index}><ResultQuestion color={value} index={index+1}/></div>
+                        }))}
                     </div>
                 </div>
             </div>
-            
         );
     }
 }
