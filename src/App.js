1e670a614dacd7784f499f35a20f9bdb13007a1d
Lea <LeaLaufner@t-online.de>
2020-03-22 11:45:52 +0100

    correctionReview edit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Imprint from "./pages/Imprint";
 import Privacy from "./pages/Privacy";
 import Credits from "./pages/Credits";
 import ReportsList from "./pages/Reports/List";
-import CorrectionReview from "./pages/CorrectionReview";
 import Sidebar from "./components/Sidebar";
+import CorrectionReview from "./pages/CorrectionReview";
+
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -16,14 +17,14 @@ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<Sidebar/>
-				<Route path={['/login', '/register', '/']} exact component={Landing} />
-				<Route path='/task' component={Task} />
-				<Route path='/dashboard' component={Dashboard} />
-				<Route path='/imprint' component={Imprint} />
-				<Route path='/privacy' component={Privacy} />
-				<Route path='/credits' component={Credits} />
-				<Route path='/reports/list' component={ReportsList} />
+                <Sidebar/>
+                <Route path={['/login', '/register', '/']} exact component={Landing} />
+                <Route path='/task' component={Task} />
+                <Route path='/dashboard' component={Dashboard} />
+                <Route path='/imprint' component={Imprint} />
+                <Route path='/privacy' component={Privacy} />
+                <Route path='/credits' component={Credits} />
+                <Route path='/reports/list' component={ReportsList} />
 				<Route path='/correctionReview' component={CorrectionReview} />
 			</Router>
 		);
