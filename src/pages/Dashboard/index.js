10f67e1e1e9b55db070e13c6e55f291acd50f8d0
RuyRun <>
2020-03-22 15:55:13 +0100

    add dashboard
    add dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import './index.css';
-<<<<<<< Updated upstream
-import Header from '../../components/Header';
 import Home_icon from '../../Assets/icons/home.svg';
 import Task_icon from '../../Assets/icons/alert-circle.svg';
-=======
 import './index.scss'
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
@@ -15,7 +11,32 @@ import users from '../../../src/Assets/icons/dashboard/users.svg';
 import text from '../../../src/Assets/icons/dashboard/file-text.svg';
 import codepen from '../../../src/Assets/icons/dashboard/codepen.svg';
 import zap from '../../../src/Assets/icons/dashboard/zap.svg';
->>>>>>> Stashed changes
+
+
+class DashboardBox extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <div className='DashbordBox'>
+			<div style={{ backgroundColor: this.props.color }} id='color'></div>
+			<div className='top'>
+				<img src={text} alt="code-icon" className='icon' />
+				<p>{this.props.name}</p>
+				<span>{this.props.subtitle}</span>
+			</div>
+			<div className='exercise'>
+				<img src={calendar} alt="calendar-icon" />
+				<p>Wöchentliche Aufgaben: {this.props.taskNumber}</p>
+			</div>
+			<div className='stats'>
+				<img src={user} alt="user-icon" /> <p>{this.props.ownPoints} Punkte</p>
+				<img src={users} alt="users-icon" /> <p>{this.props.allPoints} Punkte</p>
+			</div>
+		</div>
+	}
+}
 
 class Dashboard extends Component {
 	constructor(props) {
@@ -24,140 +45,27 @@ class Dashboard extends Component {
 	state = {};
 
 	render() {
-<<<<<<< Updated upstream
-		return (
-			<div className='main'>
-				<Header side='Dashboard' />
-				<div className='sidebar'>
-					<a href='#' className='Menu' id='Home'>
-						<img src={Home_icon} alt='Home' />
-						<span>Home</span>
-					</a>
-					<a href='#' className='Menu' id='Task'>
-						<img src={Task_icon} alt='Task' />
-						<span>Home</span>
-					</a>
-				</div>
-				<div className='content'>
-=======
+		let liste = [['#ff0000', 'Deutsch', 'Sprachen lernen - Babel', '4/5', '213', '21.323'],
+		['#202bb5', 'Mathe', 'Sprachen lernen - Babel', '4/5', '213', '21.323'],
+		['#000000', 'Informatik', 'Sprachen lernen - Babel', '4/5', '213', '21.323'],
+		['#f45e00', 'Geschichte', 'Sprachen lernen - Babel', '4/5', '213', '21.323'],
+		['#b520ab', 'Politik', 'Sprachen lernen - Babel', '4/5', '213', '21.323'],
+		['#707070', 'Physik', 'Sprachen lernen - Babel', '4/5', '213', '21.323']];
+		let listhtml = liste.map(fillhtml => {
+			return (<DashboardBox color={fillhtml[0]} name={fillhtml[1]} subtitle={fillhtml[2]}
+				taskNumber={fillhtml[3]} ownPoints={fillhtml[4]} allPoints={fillhtml[5]} />);
+		});
 		return <div className='dashboard'>
-			<div className='head'>
-				<Header side='Dashboard' />
-			</div>
-			<div className='sid'>
-				<Sidebar side='Dashboard' />
-			</div>
-
-			<div id='content'>
+			<Header side='Dashboard' />
+			<Sidebar side='Dashboard' />
+			<div id='contentdashboard'>
 				<div className='content-dashboard'>
-					<div className='box'>
-						<div id='color'></div>
-						<div className='top'>
-							<img src={text} alt="code-icon" className='icon' />
-							<p>Deutsch</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
->>>>>>> Stashed changes
-					<div className='box'>
-						<div id='color2'></div>
-						<div className='top'>
-							<img src={codeimg} alt="code-icon" className='icon' />
-							<p>Mathe</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
-					<div className='box'>
-						<div id='color3'></div>
-						<div className='top'>
-							<img src={codeimg} alt="code-icon" className='icon' />
-							<p>Informatik</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
-					<div className='box'>
-						<div id='color4'></div>
-						<div className='top'>
-							<img src={codepen} alt="code-icon" className='icon' />
-							<p>Geschichte</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
-					<div className='box'>
-						<div id='color5'></div>
-						<div className='top'>
-							<img src={codeimg} alt="code-icon" className='icon' />
-							<p>Politik</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
-					<div className='box'>
-
-						<div id='color6'></div>
-						<div className='top'>
-							<img src={zap} alt="code-icon" className='icon' />
-							<p>Physik</p>
-							<span>Sprachen lernen - Babel</span>
-						</div>
-
-						<div className='exercise'>
-							<img src={calendar} alt="calendar-icon" />
-							<p>Wöchentliche Aufgaben: 4/5</p>
-						</div>
-						<div className='stats'>
-							<img src={user} alt="user-icon" /> <p>213 Punkte</p>
-							<img src={users} alt="users-icon" /> <p>21.323 Punkte</p>
-						</div>
-					</div>
+					{listhtml}
 				</div>
 			</div>
 		</div>;
 	}
 }
+
 
 export default Dashboard;
