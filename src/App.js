478f778eba4d0bc5be043610c4dfae07e2d808d7
xR4iden <34060658+xR4iden@users.noreply.github.com>
2020-03-22 23:47:56 +0100

    Register Validierung, Routen eingefügt
    Co-authored-by: Akida31 <johannes.zunker@gmx.de>

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import './css/main.scss';
-import React, { Component } from 'react';
-import Landing from './pages/Landing';
-import Task from './pages/Task';
-import Dashboard from './pages/Dashboard';
+import "./css/main.scss";
+import React, { Component } from "react";
+import Landing from "./pages/Landing";
+import Task from "./pages/Task";
+import Dashboard from "./pages/Dashboard";
 import Imprint from "./pages/Imprint";
 import Privacy from "./pages/Privacy";
 import Credits from "./pages/Credits";
 import ReportsList from "./pages/Reports/List";
-import Sidebar from "./components/Sidebar";
-import Result from './pages/Result';
+import Result from "./pages/Result";
+//import Profile from "./pages/Profile";
+//import Settings from "./pages/Settings";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-                <Sidebar/>
-                <Route path={['/login', '/register', '/']} exact component={Landing} />
-                <Route path='/task' component={Task} />
-                <Route path='/dashboard' component={Dashboard} />
-                <Route path='/imprint' component={Imprint} />
-                <Route path='/privacy' component={Privacy} />
-                <Route path='/credits' component={Credits} />
-                <Route path='/reports/list' component={ReportsList} />
-				<Route path='/result' component={Result} />
-			</Router>
-		);
-	}
+    render() {
+        //<Route path="/profile" component={Profile}/>
+        //<Route path="/settings" component={Settings}/>
+        return (
+            <Router>
+                <Route
+                    path={["/login", "/register", "/"]}
+                    exact
+                    component={Landing}
+                />
+                <Route path="/task" component={Task} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/imprint" component={Imprint} />
+                <Route path="/privacy" component={Privacy} />
+                <Route path="/credits" component={Credits} />
+                <Route path="/reports/list" component={ReportsList} />
+                <Route path="/result" component={Result} />
+            </Router>
+        );
+    }
 }
 
 export default App;
