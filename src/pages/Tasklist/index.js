c35d28926dbe58499c8ef37cac1b294c89bf5183
Adrian Görisch <adrian.goerisch@10m.de>
2020-03-23 00:13:48 +0100

    fix and rebase

--- src/pages/Tasklist/index.js
+++ src/pages/Tasklist/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './index.scss';
 import UsersIcon from "../../components/icons/users.icon.js";
-import UserIcon from '../../components/icons/users.icon.js';
+import UserIcon from "../../components/icons/users.icon.js";
 
-{/*class Task extends React.Component{
+/*{/*class Task extends React.Component{
 	render(){
 		result(
 			<div className="Task">
@@ -14,38 +14,41 @@ import UserIcon from '../../components/icons/users.icon.js';
 		);
 	}
 }*/
-} from "../../components/icons/user.icon";
+
+/*} from "../../components/icons/user.icon";*/
 
 class Teacher_Task extends React.Component {
 
     render() {
         return (
             <div className="Tasklist_Content">
-				<div className="teacher_information">
-					<img alt='Profilbild' src="https://yt3.ggpht.com/a/AATXAJzQuI4-bNmqwbklzCjvm576_nZwSdpG5jX-Hw=s176-c-k-c0x00ffffff-no-rj-mo" />
-					<span id="teacher_name">{this.props.teacher_name}</span>
-				</div>
-		</div>
+                <div className="teacher_information">
+                    <img alt='Profilbild'
+                         src="https://yt3.ggpht.com/a/AATXAJzQuI4-bNmqwbklzCjvm576_nZwSdpG5jX-Hw=s176-c-k-c0x00ffffff-no-rj-mo"/>
+                    <span id="teacher_name">{this.props.teacher_name}</span>
+                </div>
+            </div>
         )
     }
 }
 
 
 class Tasklist extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	state = {};
+    constructor(props) {
+        super(props);
+    }
 
-	render() {
-		return <div className="Tasklist">
-				<div className='resultContentHeader'>
-                    <div className='resultLeft'>
+    state = {};
+
+    render() {
+        return <div className="Tasklist">
+            <div className='resultContentHeader'>
+                <div className='resultLeft'>
                         <span className='resultSubject'>
                             Task
                         </span>
-                    </div>
-					<div className='resultCenter'>
+                </div>
+                <div className='resultCenter'>
                         <span className='resultSubject'>
 							<div>
 								<UserIcon/>
@@ -56,18 +59,18 @@ class Tasklist extends React.Component {
 								<span>21.323</span><span>Punkte</span>
 							</div>
                         </span>
-                    </div>
-					<div className='resultRight'>
+                </div>
+                <div className='resultRight'>
                         <span className='resultSubject'>
                             <input type="search" id="site-search" name="Search" aria-label="Search" placeholder="Suche"/>
                         </span>
-                    </div>
                 </div>
-				<div id="Task-Content-List">
-					<Teacher_Task teacher_name="Hans Peter"/>
-				</div>
-		</div>;
-	}
+            </div>
+            <div id="Task-Content-List">
+                <Teacher_Task teacher_name="Hans Peter"/>
+            </div>
+        </div>;
+    }
 }
 
 export default Tasklist;
