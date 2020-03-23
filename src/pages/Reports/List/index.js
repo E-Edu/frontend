28fbde913cb54898180a930e70b4cc30be18d937
Eryothan <47895118+Eryothan@users.noreply.github.com>
2020-03-23 16:44:44 +0100

    ReportsList Bugfix

--- src/pages/Reports/List/index.js
+++ src/pages/Reports/List/index.js
@@ -54,7 +54,7 @@ class Report extends Component {
                         </div>
                         <span style={{
                             display: "flex", alignItems: "center", marginRight: 20, color: "#1C2541",
-                            width: 400, right: 0, justifyContent: "flex-end", backgroundColor: bgColor,
+                            right: 0, justifyContent: "flex-end", backgroundColor: bgColor,
                             borderWidth: 1, borderStyle: "solid", borderColor: bgColor[1], borderRadius: 10,
                             padding: "0px 5px"
                         }}>{this.state.difficulty}</span>
