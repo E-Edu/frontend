f561e10815a9523cf72798122e05b382c8fef115
Stefan <#Stefan2006>
2020-03-23 20:44:26 +0100

    Add Icons

---
+++ src/components/icons/award.icon.js
@@ -0,0 +1,34 @@
+import './index.scss';
+import React, {Component} from 'react';
+
+class AwardIcon extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        email: '',
+    };
+
+    render(props) {
+        return (
+            <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 24 24" 
+                    fill={this.props.fill || 'none'} 
+                    stroke={this.props.stroke || 'currentColor'} 
+                    stroke-width="2" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    class="feather feather-award">
+                    <circle cx="12" cy="8" r="7"/>
+                    <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"/>
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default AwardIcon;
