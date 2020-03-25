af5597f3034c5f889abbf0aab3a6508dff5193c7
Eryothan <47895118+Eryothan@users.noreply.github.com>
2020-03-25 18:29:15 +0100

    fixed sidebar in dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -53,7 +53,7 @@ class dashboard extends React.Component {
         return (
             <div id="dashboard">
                 <Header side="Dashboard" />
-                <Sidebar />
+                <Sidebar active="dashboard"/>
                 <div className="Subjekts_content">
                     <Subjekt Subject="Deutsch" Underline="Sprachen lernen - Babel" Weekendtask="4/5" Points="213" Community_Points="21.323" />
                     <Subjekt Subject="Mathe" Underline="1 x  3 macht 4 und 3 macht 9" Weekendtask="4/5" Points="213" Community_Points="21.323" />
