9164cdde0e4e75c0eb6b649259de30ea406f3511
Lea <LeaLaufner@t-online.de>
2020-03-26 22:16:54 +0100

    edited subjectTask

--- src/pages/SubjectTask/index.js
+++ src/pages/SubjectTask/index.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 
 import User from '../../assets/icons/user.svg';
 import Users from '../../assets/icons/users.svg';
+
 //import Sidebar from '../../components/Sidebar';
 
 class SubjectTask extends React.Component {
@@ -11,12 +12,21 @@ class SubjectTask extends React.Component {
         super(props);
     }
 
+    state = {
+        currentQuestion: 4,
+        maxQuestion: 10
+    };
+
+/*    add() {
+        const newValue = this.state.currentQuestion + 1;
+        this.setState({currentQuestion: newValue})
+    }*/
+
     results = ['#BA1919', '#19BA3f', '#687D9A', '#687D9A', '#19BA3f'];
 
     render() {
         return (
-                       
-            <div className='subjectTask'>
+            <div className='subjectTask' id='subjectTask'>
                 <Header side='Dashboard'/>
                 <div className='subjectTaskContainer'>
                     <div className='subjectTaskContentHeader'>
@@ -39,54 +49,45 @@ class SubjectTask extends React.Component {
                             </div>
                         </div>
                     </div>
-                    
-                </div>
-                <div className='progressDisplay'>
-                    <span>Frage 4 von 10 </span>
-                    <div className='progressBar'>
-                        <div className='progressPer' per='90'></div>
 
-                    </div>
-                    <div className='subjectTaskTitel'>
-                        <span>Äpfel und Birnen</span>
 
-                    </div>
-                    <div className='subjectTaskDescription'>
+                    <div className='progressDisplay'>
+                        <span>Frage 4 von 10 </span>
+                        <div className='progressBar'>
+                            <div className='progressPer' per='90'
+                                 style={{width: `${(this.state.currentQuestion / this.state.maxQuestion) * 100}%`}}></div>
+
+                        </div>
+                        <div className='subjectTaskTitel'>
+                            <span>Äpfel und Birnen</span>
+
+                        </div>
+                        <div className='subjectTaskDescription'>
                         <span>
                             Peter hat 3 Äpfel und 4 Birnen. Er gibt Lena 2 Äpfel und 1 Birne ab. <br/>
                              Wieviele Äpfel hat Peter noch?
                         </span>
-                    </div>
-
-                </div>
-                <div className='subjectTaskAnswer'>
-                    <div className='subjectTaskAnswerHeader'>
-                        <span> Antwort </span>
-                        <div className='subjectTaskAnswerfield'>
-                            <input type="text" placeholder='test'/>
                         </div>
-
                     </div>
-                    
-                   
-                    <div className='subjectTaskButtons'>
-                        <div className='subjectTaskSkip'>
-                            <span> Überspringen </span>
-                         </div>
-                        <div className='subjectTaskNext'>
-                            <span>Nächste Frage</span>
+                    <div className='subjectTaskAnswer'>
+                        <div className='subjectTaskAnswerHeader'>
+                            <span> Antwort </span>
+                            </div>
+                            <div className='subjectTaskAnswerfield'>
+                                <input type="text" placeholder='test'/>
+                            </div>
+                        
+                        <div className='subjectTaskButtons'>
+                            <button className='subjectTaskSkip'>
+                                Überspringen
+                            </button>
+                            <button className='subjectTaskNext' >
+                                Nächste Frage
+                            </button>
                         </div>
-
                     </div>
-                   
-
-
-
                 </div>
-
             </div>
-            
-           
         );
     }
 }
