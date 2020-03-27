a0bba940db5c0016335a209f483ee3a46519d39e
Adrian Görisch <adrian.goerisch@10m.de>
2020-03-27 14:18:14 +0100

    introducing Storybook

---
+++ src/components/icons/user.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {number,color, withKnobs} from "@storybook/addon-knobs";
+import {storiesOf} from "@storybook/react";
+import UserIcon from "./user.icon";
+
+const stories = storiesOf('UserIcon', module);
+stories.addDecorator(withKnobs);
+
+stories.add('default', () => {
+    return <UserIcon/>
+});
+
+stories.add('dynamicInput', () => {
+    const height = number("height", 24);
+    const width = number("width", 24);
+    const fill = color("fill", "#ffffff");
+    const stroke = color("stroke", "#000000");
+
+    return <UserIcon height={height} width={width} fill={fill} stroke={stroke}/>;
+});
+
+
+
