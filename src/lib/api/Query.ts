8e0551c4b63ea1073d4c849d5089941f3e908e0b
Steve Hb <stve.hb@gmail.com>
2020-03-28 14:08:55 +0100

    Removed unused parts

--- src/lib/api/Query.ts
+++ src/lib/api/Query.ts
@@ -7,63 +7,12 @@ const API_URL = "https://api.e-edu.the-morpheus.de/graphql";
 export default class Query {
 
     static client = new ApolloClient({uri: API_URL});
-    private _queue: any[] = [];
 
-    async execute(): Promise<void> {
-        for (let fun of this._queue) {
-            await fun();
-        }
-    }
-
-    authenticate(model: UserAuth): Query {
-        this.addMutation(gql`
-            mutation {
-                authenticate(userAuth: {passwordHash: "Test", key: "Max"}) {
-                    result
-                }
-            }`
-        );
-
-        return this;
-    }
-
-    jwt(): Query {
-        this.addMutation(gql`
-            mutation {
-                jwt {
-                    token
-                    status
-                }
-            }`
-        );
-
-        return this;
-
-    }
-
-    private addMutation(mutation: any) {
-        this.add(async () => {
-            let result = await Query.client.mutate({mutation: mutation});
-            console.log(result);
-        });
-    }
-
-    private addQuery(query: any) {
-        this.add(async () => {
-            let result = await Query.client.query({query: query});
-            console.log(result);
-        });
-    }
-
-    private add(fun: any) {
-        this._queue.push(fun);
-    }
-
-    public static async queryGQL(query: any) {
+    public static async queryGQL(query: any): Promise<any> {
         return Query.client.query({query: query});
     }
 
-    public static async mutationGQL(mutation: any) {
+    public static async mutationGQL(mutation: any): Promise<any> {
         return await Query.client.mutate({mutation: mutation});
     }
 
