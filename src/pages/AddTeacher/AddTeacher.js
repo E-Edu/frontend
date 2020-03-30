66dcf67256bd8193dd990a635351384092b45fdc
Steve Hb <stve.hb@gmail.com>
2020-03-30 17:40:51 +0200

    Fixed rebase conflicts

--- src/pages/AddTeacher/AddTeacher.js
+++ src/pages/AddTeacher/AddTeacher.js
@@ -22,15 +22,8 @@ class AddTeacher extends Component {
     };
 
     isValidEmail() {
-        const regex =
-<<<<<<< HEAD
-            '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])';
-        const email = document.getElementById('teacherEmail').value;
-        return email.match(regex) !== null;
-=======
-            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])";
+        const regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])";
         return this.state.email.match(regex) !== null;
->>>>>>> Implemented teacher requests
     }
 
     emailChange = (event) => {
@@ -46,71 +39,16 @@ class AddTeacher extends Component {
         if (this.state.validEmail) alert("Teacher added");
     };
 
-<<<<<<< HEAD
-    render() {
-        const pendingRequests = [
-            ['Herr Lehrer Mustermann', 'lehrer.mustermann@lehrer-mail.de'],
-            ['Herr Lehrer Mustermann2', 'lehrer.mustermann2@lehrer-mail.de'],
-        ];
-        let requestsHeadline = 'ausstehende Anfragen';
-        if (pendingRequests.length === 0) {
-            requestsHeadline = `Keine ${requestsHeadline}`;
-        }
-        const requestsText = pendingRequests.map((request) => {
-            return (
-                <div
-                    style={{
-                        position: 'relative',
-                        marginBottom: 10,
-                        display: 'flex',
-                    }}
-                    key={request[0]}>
-                    <AddRequest name={request[0]} email={request[1]} />
-                </div>
-=======
     renderRequests() {
         return this.state.pendingRequests.map((request, index) => {
             return (
                 <AddTeacherRequest key={index} name={request[0]} email={request[1]}/>
->>>>>>> Implemented teacher requests
             );
         });
     }
 
     render() {
         return (
-<<<<<<< HEAD
-            <div>
-                <div className="main">
-                    <div style={{ marginLeft: 50 }}>
-                        <h1 className="text-dark add-teacher-text-content">Lehrer hinzufügen</h1>
-                        <div style={{ display: 'flex' }}>
-                            <input
-                                type="text"
-                                className="teacher-email"
-                                placeholder="Email des Lehrers"
-                                style={{ padding: 5 }}
-                                /* className={this.state.valid } TODO bessere lösung finden */
-                                onChange={this.emailChange}
-                            />
-                            <img
-                                src={icon_user_plus}
-                                alt="user-plus-icon"
-                                style={{
-                                    border: 'solid',
-                                    borderWidth: 2,
-                                    borderRadius: 8,
-                                    marginLeft: 20,
-                                    padding: 5,
-                                    borderColor: '#1c2541',
-                                }}
-                                onClick={this.add}
-                            />
-                        </div>
-                        <h1>{requestsHeadline}</h1>
-                    </div>
-                    <div style={{ display: 'flex', flexDirection: 'column' }}>{requestsText}</div>
-=======
             <Page
                 mainTitle={Translation.t("addTeacher.title")}>
                 <div className="add-teacher">
@@ -135,7 +73,6 @@ class AddTeacher extends Component {
                 }</h1>
                 <div className="pending-requests">
                     {this.renderRequests()}
->>>>>>> Implemented teacher requests
                 </div>
             </Page>
         );
