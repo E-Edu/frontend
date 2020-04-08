56bee3fe350542506ace2d161fa007d7dbf82a8c
Michael Stangl <michaelstangl106@gmail.com>
2020-04-08 21:12:55 +0200

    Fixed styling

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,16 +1,16 @@
 describe('Login and registration test', () => {
     it('Should login', () => {
-        cy.visit('/');
-        cy.get('.btn-login').click();
-        cy.url().should('include', '/dashboard');
+        cy.visit('/')
+            .get('.btn-login').click()
+            .url().should('include', '/dashboard');
     });
     it('Should register', () => {
-        cy.visit('/');
-        cy.get('.btn-register').click();
-        cy.url().should('include', '/dashboard');
+        cy.visit('/')
+            .get('.btn-register').click()
+            .url().should('include', '/dashboard');
     });
     it('Should logout', () => {
-        cy.get('.btn-logout').click();
-        cy.url().should('include', '/');
+        cy.get('.btn-logout').click()
+            .url().should('include', '/');
     });
 });
--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,17 +1,17 @@
 describe('Footer links test', () => {
     it('Should show imprint', () => {
-        cy.visit('/');
-        cy.get('.imprint-link').click();
-        cy.url().should('include', '/imprint');
+        cy.visit('/')
+            .get('.imprint-link').click()
+            .url().should('include', '/imprint');
     });
     it('Should show privacy', () => {
-        cy.visit('/');
-        cy.get('.privacy-link').click();
-        cy.url().should('include', '/privacy');
+        cy.visit('/')
+            .get('.privacy-link').click()
+            .url().should('include', '/privacy');
     });
     it('Should show credits', () => {
-        cy.visit('/');
-        cy.get('.credits-link').click();
-        cy.url().should('include', '/credits');
+        cy.visit('/')
+            .get('.credits-link').click()
+            .url().should('include', '/credits');
     });
 });
