2596d3f0fe3b6826dd0ab824e1411ca94e82c193
Stefan <#Stefan2006>
2020-04-14 22:37:58 +0200

    Fix Storybook error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register'
   ],
-};
+}
