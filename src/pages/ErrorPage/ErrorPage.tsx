ed8a8b90e780fb47ebb9537a743b198e61af7d30
akida <47895118+Akida31@users.noreply.github.com>
2020-05-01 12:47:59 +0200

    added error 404 page

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ErrorPage.scss';
+import { Link } from 'react-router-dom';
+import ErrorIcon from '../../components/icons/error.icon';
+import { t } from '../../i18n/i18n';
+
+class ErrorPage extends React.Component {
+    render() {
+        return (
+            <div className="error404">
+                <ErrorIcon width="40%" />
+                <h1>{t.t('error.heading', 'Oops, looks like here is nothing')}</h1>
+                <p>
+                    {t.t('error.description', 'The page you are looking for does not exist! Go back to')}{' '}
+                    <Link to="/" className="error-link">
+                        {t.t('error.link', 'home')}
+                    </Link>
+                </p>
+            </div>
+        );
+    }
+}
+export default ErrorPage;
